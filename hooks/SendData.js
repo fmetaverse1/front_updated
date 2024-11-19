296d6554b489c123fecd980ecdc6f0806f105927
fmetaverse1 <fmetaverse1@gmail.com>
2024-11-20 00:29:41 +0100

    Update SendData.js

--- hooks/SendData.js
+++ hooks/SendData.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 function SendData(params) {
-    axios.post(`https://back-updated.onrender.com/create/user`, params)
+    axios.post(`https://back-updated-j9wo.onrender.com/create/user`, params)
 
 }
 
